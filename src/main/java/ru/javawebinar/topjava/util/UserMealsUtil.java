fadfe10fd2a5ff206938a6b216716352b712a3f0
stea1th <stea1th@mail.ru>
2018-06-20 23:12:28 +0200

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,13 +20,29 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000).forEach(System.out::println);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        List<UserMealWithExceed> userMealWithExceeds = new ArrayList<>();
+        mealList.stream().filter(x-> x.getDateTime().toLocalTime().isAfter(startTime) && x.getDateTime().toLocalTime().isBefore(endTime))
+                 .forEach(userMeal-> {
+             if(mealList.stream()
+                     .collect(Collectors.groupingBy((UserMeal meal)-> meal.getDateTime().toLocalDate(), Collectors.summingLong(UserMeal::getCalories)))
+                     .entrySet()
+                     .stream()
+                     .filter(entry-> entry.getValue()>caloriesPerDay)
+                     .map(Map.Entry::getKey)
+                     .collect(Collectors.toList())
+                     .contains(userMeal.getDateTime().toLocalDate())){
+                 userMealWithExceeds.add(new UserMealWithExceed(userMeal, true));
+             }else{
+                 userMealWithExceeds.add(new UserMealWithExceed(userMeal, false));
+             }
+         });
+
+         return userMealWithExceeds;
     }
 }
