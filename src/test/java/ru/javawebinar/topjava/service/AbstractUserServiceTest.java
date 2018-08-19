b0dda9c488b19be0a599d65cbf7405d205324152
stea1th <stea1th@mail.ru>
2018-08-19 18:37:29 +0200

    1.1 and 1.2

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
@@ -91,6 +92,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     }
 
     @Test
+
     public void testValidation() throws Exception {
         validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
